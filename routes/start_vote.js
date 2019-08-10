963395215d5d942a93ad3ecc3bc85ab5b1b3def3
Chandler Anderson <chandler.anderson@slalom.com>
2019-08-10 10:38:45 -0400

    Add callbacks to Slack.

--- routes/start_vote.js
+++ routes/start_vote.js
@@ -4,13 +4,17 @@ var motion = ''
   , activeMotion = ''
   , redis = require('redis')
   , motionnameText = ''
+  , motionNameForGroup = ''
   , triggerWord = ''
   , channelId = ''
+  , newGroupId = ''
   , slackRes = ''
   , rtg = ''
   , newMotionID = ''
   , userName = ''
-  , ts = Math.floor(Date.now() / 1000);
+  , ts = Math.floor(Date.now() / 1000)
+  , request = require('request')
+  , token = '';
 
 exports.post = function (req, res, next) {
 
@@ -23,31 +27,85 @@ exports.post = function (req, res, next) {
     triggerWord = req.body.trigger_word;
     channelId = req.body.channel_id;
     userName = req.body.user_name;
+    token = req.body.token;
     motionnameText = motionnameText.replace(triggerWord + ' ', '');
+    motionNameForGroup = motionnameText.replace(' ', '-').toLowerCase();
     motion = {
         "motionName": motionnameText,
         "hasSecond": false,
         "active": 1,
-        "answers": []
+        "answers": [],
+        "activity": []
     };
 
     newMotionID = Date.now();
 
     /*
      * Set new motion with the active motion id.
-     * Print confirmation and vote message.
-     */
+    */
     console.log('Setting up new motion with ID: ' + newMotionID);
-    dbActions.setMotion(newMotionID, JSON.stringify(motion), printNewMotion);
+    dbActions.setMotion(newMotionID, JSON.stringify(motion), beginPostProcessing);
 
-    function printNewMotion() {
+    function beginPostProcessing() {
         console.log('New motion is set up with the ID: ' + newMotionID);
-        dbActions.getMotion(newMotionID, confirmNewMotion);
+        createNewGroup();
     }
 
-    function confirmNewMotion(data) {
-        var printedMotion = tally.printMotion(JSON.parse(data));
-        res.json({
+    function createNewGroup() {
+        var newGroupRequestBody = {
+            form: {
+                token: token,
+                name: "motion-" + motionNameForGroup
+            }
+        };
+
+        request.post('https://slack.com/api/group.create', newGroupRequestBody, function (error, response, body) {
+            console.log(body);
+            var group = body.group;
+            newGroupId = groupId;
+            sendInitialMessage();
+        });
+
+        res.json(secondResponse);
+    }
+
+    function sendInitialMessage() {
+        var printedMotion = tally.printMotion(JSON.parse(motion));
+        var initMessageRequestBody = {
+            form: {
+                "token": token,
+                "channel": newGroupId,
+                "text": userName + ' has brought a motion to the floor.',
+                "attachments": [
+                    {
+                        "text": printedMotion,
+                        "fallback": "You are unable to second.",
+                        "callback_id": "second_" + newMotionID,
+                        "color": "#3AA3E3",
+                        "attachment_type": "default",
+                        "actions": [
+                            {
+                                "name": "second",
+                                "text": "Second",
+                                "type": "button",
+                                "value": "second"
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
+
+        request.post('https://slack.com/api/chat.postMessage', initMessageRequestBody, function (error, response, body) {
+          // Sends welcome message
+            handleResults();
+        });
+
+    }
+
+    function handleResults() {
+        var printedMotion = tally.printMotion(JSON.parse(motion));
+        var secondResponse = {
             "text": userName + ' has brought a motion to the floor.',
             "attachments": [
                 {
@@ -66,7 +124,9 @@ exports.post = function (req, res, next) {
                     ]
                 }
             ]
-        });
+        };
+
+        res.json(secondResponse);
     }
 
 };
