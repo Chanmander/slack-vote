be3878061b951cba3edcf2e45d1afde42de70670
Chandler Anderson <chandler.anderson@slalom.com>
2019-08-09 21:11:00 -0400

    Set up seconding action.

--- routes/take_action.js
+++ routes/take_action.js
@@ -3,6 +3,8 @@ var motion = ''
     , dbActions = require('./../persist.js')
     , tally = require('./../tally.js')
     , redis = require('redis')
+    , actionType = ''
+    , motionId = ''
     , activeMotion = ''
     , slackRes = ''
     , motionResults = ''
@@ -20,9 +22,75 @@ var motion = ''
  * Handle voting actions.
  */
 exports.post = function (req, res, next) {
-    console.log(req.body);
-
+    var body = req.body;
+    var payload = JSON.parse(body.payload);
+    var user = payload.user;
     userName = user.name;
 
-    res.json({text: 'Got a second from ' + userName + '!'});
+    console.log(payload);
+
+    var callbackIdParts = payload.callback_id.split('_');
+    actionType = callbackIdParts[0];
+    motionId = callbackIdParts[1];
+
+    /*
+     * Get motion with ID.
+     */
+    dbActions.getMotion(motionId, setData);
+
+    function setData(motion_string) {
+        console.log('Motion prior to submission: ' + motion_string);
+        if (motion_string) {
+            data = JSON.parse(motion_string);
+            if (data.active === 1) {
+                console.log('Action type: ' + actionType);
+                switch (actionType) {
+                    case 'second':
+                        data.hasSecond = true;
+                        console.log('Seconded motion ' + motionId + '!');
+                        dbActions.setMotion(motionId, JSON.stringify(data), handleResults);
+                        break;
+                    default:
+                        res.json({
+                            text: 'Did not understand the action.'
+                        });
+                }
+            } else {
+                res.json({
+                    text: 'Motion inactive.' + data
+                });
+            }
+        }
+    }
+
+    function handleResults(data) {
+        slackRes += '\n' + 'Got a second from ' + userName + '!';
+        var printedMotion = tally.printMotion(data);
+        res.json({
+            "text": slackRes,
+            "attachments": [
+                {
+                    "text": printedMotion,
+                    "fallback": "You are unable to vote.",
+                    "callback_id": "vote_" + motionId,
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "yes",
+                            "text": "Yay",
+                            "type": "button",
+                            "value": "yes"
+                        },
+                        {
+                            "name": "no",
+                            "text": "Nay",
+                            "type": "button",
+                            "value": "yes"
+                        }
+                    ]
+                }
+            ]
+        });
+    }
 }
